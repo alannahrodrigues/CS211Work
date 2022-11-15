55608b3e37ecbc577accdf40e6bbbef9c3cc239b
Alannah Rodrigues <106991864+alannahrodrigues@users.noreply.github.com>
2022-11-15 05:43:12 +0000

    new file

--- .ccls-cache/@home@runner@TalkativeWoozyMethod/main.c
+++ .ccls-cache/@home@runner@TalkativeWoozyMethod/main.c
@@ -3,6 +3,8 @@
 int main(void) {
   printf("Trying GitHub!\n");
   printf("Version2\n");
+
+  printf("trial for git diff\n");
   
   return 0;
 }
---
+++ newMain.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("New Main.c\n");
+  
+  return 0;
